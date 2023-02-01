5d1694b9a7b8cb4006bc0c67c754c642e89a6d54
Ricardo <zunosan.ricardo506@gmail.com>
2023-01-31 22:30:16 -0600

    Iniciando el proyecto

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.jsx',
+    './src/**/*.js'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
