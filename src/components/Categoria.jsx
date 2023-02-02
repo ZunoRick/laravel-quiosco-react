80635aeef95c6435a4c50d05740b52fc13304097
Ricardo <zunosan.ricardo506@gmail.com>
2023-02-01 22:36:12 -0600

    Agregando un Context

--- src/components/Categoria.jsx
+++ src/components/Categoria.jsx
@@ -1,9 +1,18 @@
+import useQuiosco from "../hooks/useQuiosco"
 
 export default function Categoria({ categoria }) {
+  const { handleClickCategoria, categoriaActual } = useQuiosco()
   const { id, icono, nombre } = categoria
+
+  const resaltarCategoriaActual = () => categoriaActual.id === id ? "bg-amber-400" : "bg-white"
+
   return (
-    <div
-      className="flex items-center gap-4 border w-full p-3 hover:bg-amber-400 cursor-pointer"
+    <button
+      className={`${ resaltarCategoriaActual() }
+      flex items-center gap-4 border 
+      w-full p-3 hover:bg-amber-400 cursor-pointer`}
+      type="button"
+      onClick={ () => handleClickCategoria(id) }
     >
       <img 
         src={ `/img/icono_${icono}.svg` }
@@ -15,6 +24,6 @@ export default function Categoria({ categoria }) {
       >
         { nombre }
       </p>
-    </div>
+    </button>
   )
 }
