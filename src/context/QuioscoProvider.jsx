c4f96ab545a1814b73729aff5c1de773a96f2870
Ricardo <zunosan.ricardo506@gmail.com>
2023-02-08 23:03:16 -0600

    Agregando modal para modificar cantidades

--- src/context/QuioscoProvider.jsx
+++ src/context/QuioscoProvider.jsx
@@ -6,10 +6,20 @@ const QuioscoContext = createContext()
 const QuioscoProvider = ({ children }) => {
   const [categorias,  setCategorias] = useState(categoriasDB);
   const [categoriaActual, setCategoriaActual] = useState(categorias[0])
+  const [modal, setModal] = useState(false)
+  const [producto, setProducto] = useState({})
 
   const handleClickCategoria = id =>{
     const categoria = categorias.filter(categoria => categoria.id === id)[0]
     setCategoriaActual(categoria)
+  }
+
+  const handleClickModal = () =>{
+    setModal(!modal)
+  }
+
+  const handleSetProducto = producto =>{
+    setProducto(producto)
   }
 
   return (
@@ -17,7 +27,11 @@ const QuioscoProvider = ({ children }) => {
       value= {{
         categorias,
         categoriaActual,
-        handleClickCategoria
+        handleClickCategoria,
+        handleClickModal,
+        modal,
+        producto,
+        handleSetProducto
       }}
     >
       { children }
